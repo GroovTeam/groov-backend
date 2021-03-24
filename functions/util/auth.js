2c061425e7e8e75f8a6651d43a87bdd184512798
Marshall <marshallmason@knights.ucf.edu>
2021-03-23 20:59:43 -0400

    email registration verification

--- functions/util/auth.js
+++ functions/util/auth.js
@@ -9,22 +9,26 @@ module.exports = (req, res, next) => {
   } 
   else {
     console.error('No token');
-    return res.status(403).json({ error: 'Unauthorized' });
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
   admin.auth().verifyIdToken(idToken)
       .then((decodedToken) => {
+        if (!decodedToken.email_verified && !req.body.bypass) 
+          throw new Error('User email not verified');
+
         req.user = decodedToken;
         return db.collection('users')
         .where('uid', '==', req.user.uid)
         .get();
       })
       .then((data) => {
-        req.user.username = data.docs[0].data().username;
+        // req.user.username = data.docs[0].data().username;
+        req.user = data.docs[0].data();
         return next();
       })
       .catch((err) => {
         console.error('Error while verifying token', err);
-        return res.status(403).json(err);
+        return res.status(403).json({ message: err.message });
       });
 };
