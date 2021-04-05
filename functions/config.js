2e615ff1b8127b99a31f9ff94df1c1f72a0beed2
pjcamara <pjcamara11@gmail.com>
2021-04-05 00:35:51 -0400

    Add and route YouTube API search endpoint

--- functions/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: 'apiKey',
-  authDomain: 'authDomain',
-  projectId: 'projectId',
-  storageBucket: 'storageBucket',
-  messagingSenderId: 'messagingSenderId',
-  appId: 'appId',
-  measurementId: 'measurementId',
-};
---
+++ functions/routes/youtube/search.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+// const { db, admin, firebase } = require('../../util/admin');
+const fetch = require('node-fetch');
+const { apiKey } = require('../../config');
+
+const baseURL = 'https://www.googleapis.com/youtube/v3';
+
+
+// TODO: Configure URL parameters as we need
+
+// Retrives a batched query of YT videos based on keywords. Can be configured further as needed.
+router.post('/:query', (req, res) => {
+  const query = req.params.query;
+  const apiURL = `${baseURL}/search?part=snippet&key=${apiKey}&q=${query}&maxResults=20&order=viewCount&type=video`;
+
+  // Fetch YouTube videos based on query (TODO: Cache into db for reuse)
+  fetch(apiURL)
+    .then(results => {
+      return results.json();
+    })
+    .then(data => {
+      // Store video url into video object
+      const items = data.items;
+      items.forEach(video => {
+        const videoID = video.id.videoId;
+        video.snippet['videoURL'] = `https://www.youtube.com/watch?v=${videoID}`;
+      });
+
+      return res.json(data);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.send(404);
+    });
+});
+
+module.exports = router;
