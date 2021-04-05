a4d7b23ddd81f54de0fbad43a40bcc4cc2295bd2
Pablo C <42627320+pjcamara@users.noreply.github.com>
2021-04-05 01:51:29 -0400

    Add dummy config file to satisfy emulator

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	apiKey: 'apiKey',
+	authDomain: 'authDomain',
+	projectId: 'projectId',
+	storageBucket: 'storageBucket',
+	messagingSenderId: 'messagingSenderId',
+	appId: 'appId',
+	measurementId: 'measurementId'
+};
